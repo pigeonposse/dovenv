7682d8044120020c74e8bd8d4edf770903f99e35
angelespejo <angelespejo13@gmail.com>
2025-02-17 06:36:00 +0100

    :bug: fix(plugin): fix lint

--- packages/plugin/lint/src/staged.ts
+++ packages/plugin/lint/src/staged.ts
@@ -16,7 +16,10 @@ export class StagedLint extends LintSuper<LintStagedConfig> {
 		if ( !( await this.utils.ensureOpts( { input: this.opts } ) ) ) return
 
 		console.debug( { lintStagedConf: this.opts } )
-		const res = await lintStaged( { config: this.opts } )
+		const res = await lintStaged( {
+			config : this.opts,
+			shell  : true,
+		} )
 		if ( !res ) throw Error( 'Unsuccessful response 😢' )
 
 	}
