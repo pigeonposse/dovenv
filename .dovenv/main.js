e08332dffe4114320b03cb47d5db555fdd9a0ad4
angelespejo <angelespejo13@gmail.com>
2025-02-17 06:27:39 +0100

    :bug: fix(plugin): fix lint

--- .dovenv/main.js
+++ .dovenv/main.js
@@ -3,5 +3,9 @@ import { defineConfig }             from '../packages/core/dist/main.mjs'
 import { pigeonposseMonorepoTheme } from '../packages/theme/pigeonposse/dist/main.mjs'
 
 export default defineConfig(
-	pigeonposseMonorepoTheme( { core } ),
+	pigeonposseMonorepoTheme( {
+		core,
+		// must be added beacuse default value "dovenv lint eslint --fix --silent" because is not installed
+		lint : { staged: { '**/*.{js,ts,jsx,tsx,json}': 'pnpm --silent . lint eslint --fix --silent' } },
+	} ),
 )
